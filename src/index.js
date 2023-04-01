c8d59401d1a82438b5eee70234cbd0a6abc270b8
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-04-01 05:42:25 +0200

    Implement a function for deleting a task

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import './style.css';
-
-import { addNew } from './interactions';
-
 export const arr = [];
-const container = document.getElementById('tasks-list');
+export let container = document.getElementById('tasks-list');
 const addInput = document.getElementById('addInput');
 const addBtn = document.getElementById('addbtn');
+export let arrayIndex = 0;
 
-function removeAllTasks(parent) {
+export function removeAllTasks(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
@@ -28,16 +25,42 @@ addInput.addEventListener('keypress', (event) => {
   }
 });
 
-function component() {
-  arr.forEach((card) => {
+export function component() {
+  arr.forEach((card, n) => {
     const element = document.createElement('li');
-    element.classList.add(`i${card.index}`);
+    element.setAttribute('id', `i${card.index}`);
     element.innerHTML += `
       <input type="checkbox" name="task${card.index}">
       <label for="task${card.index}">${card.description}</label>
       <textarea  id="task${card.index}" name="inputT" class="inputT">${card.description}</textarea>
+      <button type="button" id="removeTask">
+      <span id='delete-${n}' class="material-symbols-outlined  delete-ic">delete</span>
       <span class="material-symbols-outlined ic-vDots">more_vert</span>
+      </button>
     `;
+    element.addEventListener('mouseenter', () => {
+      const deleteIC = document.getElementById(`delete-${n}`);
+      console.log(deleteIC);
+      if (deleteIC) {
+        deleteIC.style.display = 'block';
+      }
+      element.querySelector('.ic-vDots').style.display = 'none';
+      element.style.backgroundColor = 'yellow';
+    });
+    element.addEventListener('focusout', () => {
+      const deleteIC = document.getElementById(`delete-${n}`);
+      console.log(deleteIC);
+      if (deleteIC) {
+        deleteIC.style.display = 'none';
+      }
+      element.querySelector('.ic-vDots').style.display = 'block';
+      element.style.backgroundColor = 'white';
+    });
+    element.querySelector('#removeTask').addEventListener('click', () => {
+      arrayIndex = `${card.index}` - 1;
+      deletingTask();
+      element.querySelector('.ic-vDots').style.display = 'none';
+    });
     container.appendChild(element);
   });
 }
@@ -45,3 +68,6 @@ function component() {
 window.onload = () => {
   component();
 };
+import './style.css';
+
+import { addNew, deletingTask } from './interactions';
--- src/interactions.js
+++ src/interactions.js
@@ -1,4 +1,4 @@
-import { arr } from './index';
+import { arr, arrayIndex, component, container, removeAllTasks } from './index';
 
 export function addNew() {
   const description = document.getElementById('addInput').value;
@@ -10,10 +10,15 @@ export function addNew() {
       index: properIndex,
     };
     arr.push(task);
-  } else {
   }
 }
 
-function deletingTask() {}
-
-function editingTask() {}
+export function deletingTask() {
+  arr.splice(arrayIndex, 1);
+  arr.forEach((element, index) => {
+    element.index = index + 1;
+  });
+  console.log(arr);
+  removeAllTasks(container);
+  component();
+}
